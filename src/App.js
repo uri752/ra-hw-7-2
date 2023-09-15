29d9bced542face003a9ff06c537a8999f44dd07
uri752 <uri752blag@mail.ru>
2023-09-15 19:52:10 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function withPopular(Component) {
+    class NewComponent extends React.Component {
+        render() {
+            if (this.props.views > 1000) {
+                return <Popular>
+                            <Component {...this.props}/>
+                        </Popular>
+            } else if (this.props.views <100) {
+                return <New>
+                            <Component {...this.props}/>
+                       </New>
+            } else {
+                return <Component {...this.props}/>
+            }
+        } 
+    }
+        return NewComponent;
+    }
+
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':{
+                const HOC = withPopular(Video);    
+                return (                    
+                    <HOC {...item} />
+                );}
+
+            case 'article':{
+                const HOC = withPopular(Article);    
+                return (
+                    <HOC {...item} />
+                );}
+        }
+    });
+};
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 500
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+        <List list={list} />
+    );
 }
-
-export default App;
